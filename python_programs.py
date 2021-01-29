6ca618482e02ba68fae10c2b40424d14c9b4676b
kanakshilledar <56501211+kanakshilledar@users.noreply.github.com>
2021-01-29 10:52:16 +0530

    rectified some typos

--- python_programs.py
+++ python_programs.py
@@ -322,9 +322,9 @@ while True:
 #     * Print out all of the keys in alphabetical order
 #     * Print all the months in alphabetical order
 
-year = {'January' : 32, 'February' : 28, 'March' : 31, 'April' : 30, 
-    'May' : 32, 'June' : 30, 'July' : 31, 'August' : 31, 'September' : 30,
-    'October' : 32, 'November' : '30', 'December' : 31}
+year = {'January' : 31, 'February' : 28, 'March' : 31, 'April' : 30, 
+    'May' : 31, 'June' : 30, 'July' : 31, 'August' : 31, 'September' : 30,
+    'October' : 31, 'November' : '30', 'December' : 31}
 
 
 # input month name
